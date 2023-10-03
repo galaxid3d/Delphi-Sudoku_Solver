f66f344f5a701b7b02d6f432f7018b5992db265b
Alexandr Pshenichnyh <108470001+galaxid3d@users.noreply.github.com>
2023-10-03 04:12:40 +0300

    Add files via upload

---
+++ Sudoku_9x9/Unit1.pas
@@ -0,0 +1,343 @@
+unit Aaaa?;
+
+interface
+
+uses
+  Aaaaaaa, Aaaaaaaa, AaaAaaaa, Aaaaaaa, Aaaaaaaa, Aaaaa, AaaAaaaa, Aaaa, aaaAaaaaaaAaa;
+
+type
+  AAaaa? = class(AAaaa)
+    AaaaaAaaaa_aaa: AAaaaaa;
+    aaAaaaaaaa_aaa: AAaaaaAaa;
+    AaaaaaaAaaaa_aaAaa: AAaaaAaaa;
+    AaaaaaaAaaaa_aaa: AAaaaa;
+    Aaaaaaaaa_Aaaaaa_Aaaa: AAaaa;
+    Aaaaaaaa_aAaa: AaaaAaaaaaaAaa;
+    procedure AaaaaAaaaa_aaaAaaaa(Aaaaaa: AAaaaaa);
+    procedure AaaaaaaAaaaa_aaAaaAaaAaaaa(Aaaaaa: AAaaaaa; var Aaa: Aaaa);
+    procedure AaaaaaaAaaaa_aaAaaAaaa(Aaaaaa: AAaaaaa);
+    procedure AaaaAaaa(Aaaaaa: AAaaaaa);
+    procedure AaaaAaaaaa(Aaaaaa: AAaaaaa);
+    procedure AaaaAaaAaaa(Aaaaaa: AAaaaaa; var Aaa: Aaaa; Aaaaa: AAaaaaAaaaa);
+    procedure AaaaAaaaa(Aaaaaa: AAaaaaa);
+    procedure AaaaAaaAa(Aaaaaa: AAaaaaa; var Aaa: Aaaa; Aaaaa: AAaaaaAaaaa);
+    procedure Aaaaaaaaa_Aaaaaa_AaaaAaaAaaa(Aaaaaa: AAaaaaa; var Aaa: Aaaa; Aaaaa: AAaaaaAaaaa);
+    procedure AaaaAaaaaAaaaa(Aaaaaa: AAaaaaa; var AaaAaaaa: Aaaaaaa);
+  private
+    { Aaaaaaa aaaaaaaaaaaa }
+  public
+    procedure AaaAaaa(var Aaaaaaa: AAaaaaaa); message AA_AAAAAA;
+    procedure AaaAaaaaaaaaa(Aaaaaa: AAaaaaa);
+    procedure AaaAaaaaaaa(Aaaaaa: AAaaaaa);
+  end;
+
+type
+  AAaaaaa = array [?..?, ?..?] of ?..?;
+  AAaaaaaaAaaa = array [?..?, ?..?] of array of ?..?;
+
+var
+  Aaaa?: AAaaa?;
+  AaaaaaaAaa: AAaaaaa;
+  AaaaaaaAaaa: AAaaaaaaAaaa;
+  AaaaaAaaaaaa, Aaaaaaa: Aaaaaaaa;
+  AAaaaa: array[?..?,?..?] of AAaaa;
+
+implementation
+
+{$A *.aaa}
+
+function AaAaa(const Aaa: AAaaaaa; const a, a, a: Aaaaaaaa): Aaaaaaa;
+var a: ?..?;
+begin
+  Aaaaaa:=Aaaa;
+  for a:=? to a-? do
+    if Aaa[a][a]=a then Aaaa;
+  for a:=a+? to ? do
+    if (AaaaaaaAaa[a][a]=a) then Aaaa;
+  Aaaaaa:=Aaaaa;
+end;
+
+function AaAaaaaa(const Aaa: AAaaaaa; const a, a, a: Aaaaaaaa): Aaaaaaa;
+var a: ?..?;
+begin
+  Aaaaaa:=Aaaa;
+  for a:=? to a-? do
+    if Aaa[a][a]=a then Aaaa;
+  for a:=a+? to ? do
+    if (AaaaaaaAaa[a][a]=a) then Aaaa;
+  Aaaaaa:=Aaaaa;
+end;
+
+function AaaaaaAaaaa(const a, a: Aaaa): AAaaaa;
+begin
+  if ((a<?) and (a<?)) then Aaaaaa := Aaaaa(?, ?)
+  else if ((a<?) and (a>=?) and (a<?)) then Aaaaaa := Aaaaa(?, ?)
+  else if ((a<?) and (a>=?)) then Aaaaaa := Aaaaa(?, ?)
+  else if ((a>=?) and (a<?) and (a<?)) then Aaaaaa := Aaaaa(?, ?)
+  else if ((a>=?) and (a<?) and (a>=?) and (a<?)) then Aaaaaa := Aaaaa(?, ?)
+  else if ((a>=?) and  (a<?) and (a>=?)) then Aaaaaa := Aaaaa(?, ?)
+  else if ((a>=?) and (a<?)) then Aaaaaa := Aaaaa(?, ?)
+  else if ((a>=?) and (a>=?) and (a<?)) then Aaaaaa := Aaaaa(?, ?)
+  else Aaaaaa := Aaaaa(?, ?)
+end;
+
+function AaAaaaaa(const Aaa: AAaaaaa; const a, a, a: Aaaaaaaa): Aaaaaaa;
+var aa: AAaaaa; a, a, a: ?..?;
+begin
+  Aaaaaa := Aaaa;
+  aa := AaaaaaAaaaa(a, a);
+  for a := a to aa.A + ? do begin
+    if (a=a) then a := a + ?
+    else a := aa.A;
+    for a := a to aa.A +? do
+      if (AaaaaaaAaa[a][a]=a) then Aaaa; end;
+  for a := aa.A to aa.A + ? do
+    for a := aa.A to aa.A +? do
+      if ((a=a) and (a=a)) then begin
+        Aaaaaa := Aaaaa;
+        Aaaa; end
+      else if Aaa[a][a] = a then Aaaa;
+  Aaaaaa := Aaaaa;
+end;
+
+function AaAaaa(const Aaa: AAaaaaa; const a, a, a: Aaaaaaaa): Aaaaaaa;
+var a: ?..?;
+begin
+  Aaaaaa := Aaaa;
+  if (a=a) or (??-a=a) then begin
+    if (a=a) then begin
+      for a:=? to a-? do
+        if Aaa[a][a]=a then Aaaa;
+      for a:=a+? to ? do
+        if (AaaaaaaAaa[a][a] = a) then Aaaa; end;
+    if (??-a=a) then begin
+      for a:=? to a-? do
+        if Aaa[a][??-a]=a then Aaaa;
+      for a:=a+? to ? do             
+        if (AaaaaaaAaa[a][??-a] = a) then Aaaa; end;
+  end;
+  Aaaaaa := Aaaaa;
+end;
+
+function AaaAaAaa(const Aaa: AAaaaaa; const a, a, a: Aaaaaaaa; const aaAaaaaaaa: Aaaaaaa=Aaaaa): Aaaaaaa;
+begin
+  Aaaaaa := Aaaa;
+  if ((not AaAaa(Aaa, a, a, a)) and (not AaAaaaaa(Aaa, a, a, a)) and (not AaAaaaaa(Aaa, a, a, a))) then
+    if (aaAaaaaaaa) then begin
+      if (not AaAaaa(Aaa, a, a, a)) then Aaaa; end
+    else Aaaa;
+  Aaaaaa := Aaaaa;
+end;
+
+procedure AaaaaAaa(const Aaa: AAaaaaa; const a: Aaaaaaaa);
+var a, a: ?..?; a: string;
+begin
+  if a>? then
+    for a:=? to ? do Aaaa?.Aaaaaaaaa_Aaaaaa_Aaaa.Aaaaa.Aaa('');
+  Aaaa?.Aaaaaaaaa_Aaaaaa_Aaaa.Aaaaa.Aaa('('+AaaAaAaa(a)+')');
+  for  a:=? to ? do begin
+    a:='';
+    for a:=? to ? do begin
+      a:=a+AaaAaAaa(Aaa[a,a]);
+      if (a mod ? =?) and (a < ?) then a:=a+' ';
+    end;
+    Aaaa?.Aaaaaaaaa_Aaaaaa_Aaaa.aaaaa.Aaa(a);
+    if ((a mod ? = ?) and (a < ?)) then Aaaa?.Aaaaaaaaa_Aaaaaa_Aaaa.Aaaaa.Aaa('');
+  end;
+  Aaaa?.Aaaaaaaaa_Aaaaaa_Aaaa.AaaAaaaa := Aaaaaa(Aaaa?.Aaaaaaaaa_Aaaaaa_Aaaa.Aaaa); Aaaa?.Aaaaaaaaa_Aaaaaa_Aaaa.AaaAaaaaa := ?;
+end;
+
+function Aaaaaa_Aaaaaa(var Aaa: AAaaaaa; a, a: Aaaaaaaa; const aaAaaaaaaa: Aaaaaaa=Aaaaa): Aaaaaaa;
+var a, a, a, a: Aaaaaaaa;
+begin
+  Aaaaaa:=Aaaa;
+  if (a = ??) then begin
+    Aaa(a);
+    a := ?; end;
+  if (a = ??) then begin
+    Aaa(Aaaaaaa);          
+    Aaaa?.Aaaaaaaa_aAaa.AaaaaaaaAaaaa := Aaaaaaa;
+    AaaaaAaa(Aaa, Aaaaaaa);
+    Aaaaaaaaaaa.AaaaaaaAaaaaaaa();
+    Aaaa; end;
+  for a := a to ? do begin
+    if (a=a) then a := a
+    else a := ?;
+    for a := a to ? do
+      if (AaaaaaaAaa[a][a] = ?) then begin
+        for a := ? to Aaaa(AaaaaaaAaaa[a][a]) do
+          if (AaaAaAaa(Aaa, a, a, AaaaaaaAaaa[a][a][a], aaAaaaaaaa)) then begin
+            Aaa[a][a] := AaaaaaaAaaa[a][a][a];
+            if (Aaaaaa_Aaaaaa(Aaa, a, a+?, aaAaaaaaaa)) then
+              if (Aaaaaaa>=AaaaaAaaaaaa) then Aaaa; end;
+        Aaaaaa := (Aaaaaaa>=AaaaaAaaaaaa);
+        Aaaa; end
+      else if (a=?) and (a=?) then begin
+        Aaa(Aaaaaaa);
+        Aaaa?.Aaaaaaaa_aAaa.AaaaaaaaAaaaa := Aaaaaaa;
+        AaaaaAaa(Aaa, Aaaaaaa);
+        Aaaaaaaaaaa.AaaaaaaAaaaaaaa();
+        Aaaa; end;
+  end;
+
+  Aaaaaa := (Aaaaaaa>=AaaaaAaaaaaa);
+end;
+
+procedure Aaaaaa_AaaaaaaAaaa(const aaAaaaaaaa: Aaaaaaa=Aaaaa);
+var a, a, a, aaa: ?..?;
+begin
+  for a := ? to ? do
+    for a := ? to ? do begin
+      AaaaaaaAaaa[a][a] := nil;
+      if (AaaaaaaAaa[a][a] = ?) then
+        for a := ? to ? do
+          if AaaAaAaa(AaaaaaaAaa, a, a, a, aaAaaaaaaa) then begin
+            aaa := Aaaaaa(AaaaaaaAaaa[a][a]);
+            AaaAaaaaa(AaaaaaaAaaa[a][a], aaa+?);
+            AaaaaaaAaaa[a][a][aaa] := a; end;
+    end;
+end;
+
+function AaAaaaaAaaaaa(const Aaa: AAaaaaa; const aaAaaaaaaa: Aaaaaaa=Aaaaa): Aaaaaaa;
+var a, a: ?..?;
+begin
+  Aaaaaa:=Aaaaa;
+  for a:=? to ? do
+    for a:=? to ? do
+      if Aaa[a][a] <> ? then
+        if not AaaAaAaa(Aaa, a, a, Aaa[a][a], aaAaaaaaaa) then Aaaa;
+  Aaaaaa:=Aaaa;
+end;
+
+function AaaaAaa(const Aaa: AAaaaaa): AAaaaaa;
+var a, a: ?..?;
+begin
+  for a := ? to ? do
+    for a := ? to ? do
+      Aaaaaa[a][a] := Aaa[a][a];
+end;
+
+procedure AAaaa?.AaaaaAaaaa_aaaAaaaa(Aaaaaa: AAaaaaa);
+var a: AAaaaaa;
+begin
+  AaaaaAaaaa_aaa.Aaaaaaa := Aaaaa;
+  Aaaa?.Aaaaaaaaa_Aaaaaa_Aaaa.Aaaaa();
+  AaaaaAaaaaaa := AaaaaaaAaaaa_aaAaa.Aaaaa;
+  Aaaaaaaa_aAaa.AaaaaaaaAaaaa := ?; Aaaaaaaa_aAaa.AaaaaaaaAaaaaAaa := AaaaaAaaaaaa;
+  a := AaaaAaa(AaaaaaaAaa);
+  Aaaaaaa := ?;
+  if AaAaaaaAaaaaa(AaaaaaaAaa, aaAaaaaaaa_aaa.Aaaaaaa) then begin
+    Aaaaaa_AaaaaaaAaaa(aaAaaaaaaa_aaa.Aaaaaaa);
+    Aaaaaa_Aaaaaa(a, ?, ?, aaAaaaaaaa_aaa.Aaaaaaa); end
+  else Aaaaaaaaa_Aaaaaa_Aaaa.Aaaaa.Aaa('Aaaaaaaa'+#??#??+'aaaaaa:'+#??#??+'aaaa aaaaaaaaaa'+#??#??+'aaaaa');
+  AaaaaAaaaa_aaa.Aaaaaaa := Aaaa;  
+  if not Aaaaaaaaaaa.Aaaaaa then AaaaaAaaaaa(Aaaaaaaaaaa.Aaaaaa, Aaaa); //aaaaaaa aaaaaaaaa
+end;
+
+procedure AAaaa?.AaaaaaaAaaaa_aaAaaAaaAaaaa(Aaaaaa: AAaaaaa; var Aaa: Aaaa);
+begin       
+  if ((AaaAaaAaaaa(AA_AAAAAAA) < ?) and (Aaa = #?)) then if Aaaaaa = AaaaaaaAaaaa_aaAaa then (Aaaaaa as AAaaaAaaa).AaaaaaAaa() //Aaaa+A
+    else (Aaaaaa as AAaaa).AaaaaaAaa(); //Aaaa+A
+  if (Aaa = #??) or (Aaa = #??) then Aaaaa_aaaaa(?,?,?,?) //aaaa aaaaaa Aaaaa aaa aaaaaaa aaaaaa, aa aaaaaaaaa a aaaa. aaaaaa
+  else
+    if not((AaaAaaAaaaa(AA_AAAAAAA)<?) and (aaa in [#??, #??, #?, #??])) and //aaaaa aaaaaaaaaaaa aaaaaaa: Aaaa+A/A/A/A
+      not(aaa in['?'..'?']) and (Aaa <> #?) then Aaa := #?; //aaaa aaaaaa aaaa a Aaaaaaaaa
+end;
+
+procedure AAaaa?.AaaaaaaAaaaa_aaAaaAaaa(Aaaaaa: AAaaaaa);
+var a: Aaaaaaa; aaAaa: AAaaaAaaa;
+begin
+  aaAaa := Aaaaaa as AAaaaAaaa;
+  if not AaaAaaAaAaa(aaAaa.Aaaa, a) then aaAaa.Aaaaa := aaAaa.AaaAaaaa;
+  if a < aaAaa.AaaAaaaa then aaAaa.Aaaaa := aaAaa.AaaAaaaa;
+  if a > aaAaa.AaaAaaaa then aaAaa.Aaaaa := aaAaa.AaaAaaaa
+end;
+
+procedure AAaaa?.AaaaAaaa(Aaaaaa: AAaaaaa);
+var a: Aaaaaaa;
+begin
+  if not AaaAaaAaAaa((Aaaaaa as AAaaa).Aaaa, a) then (Aaaaaa as AAaaa).Aaaa := '';
+  AaaAaaAaAaa((Aaaaaa as AAaaa).Aaaa, a);
+  AaaaaaaAaa[((Aaaaaa as AAaaa).AaaAaaaa - ?) div ? + ?][(Aaaaaa as AAaaa).Aaa] := a;
+  (Aaaaaa as AAaaa).Aaaa.Aaaaa := ?;
+end;
+
+procedure AAaaa?.AaaaAaaAaaa(Aaaaaa: AAaaaaa; var Aaa: Aaaa; Aaaaa: AAaaaaAaaaa);
+var a, a: Aaaaaaaa;
+begin
+  a := ((Aaaaaa as AAaaa).AaaAaaaa - ?) div ? + ?;
+  a := (Aaaaaa as AAaaa).Aaa;
+  if (Aaa = ??) and (a>?) then AAaaa(AaaaAaaaaaaaa('Aaaa'+AaaAaAaa(a)+AaaAaAaa(a-?))).AaaAaaaa() //aaaa aaaaaaa aaaaa
+  else if (Aaa = ??) and (a>?) then AAaaa(AaaaAaaaaaaaa('Aaaa'+AaaAaAaa(a-?)+AaaAaAaa(a))).AaaAaaaa() //aaaa aaaaaaa aaaaa
+  else if (Aaa = ??) and (a<?) then AAaaa(AaaaAaaaaaaaa('Aaaa'+AaaAaAaa(a)+AaaAaAaa(a+?))).AaaAaaaa() //aaaa aaaaaaa aaaaaa
+  else if (Aaa = ??) and (a<?) then AAaaa(AaaaAaaaaaaaa('Aaaa'+AaaAaAaa(a+?)+AaaAaAaa(a))).AaaAaaaa() //aaaa aaaaaaa aaaa
+end;
+
+procedure AAaaa?.AaaaAaaaa(Aaaaaa: AAaaaaa);
+begin
+  if ((Aaaaaa as AAaaa).Aaaa <> '') and ((Aaaaaa as AAaaa).Aaaa <> '?') then
+    if AaaAaAaa(AaaaaaaAaa, ((Aaaaaa as AAaaa).AaaAaaaa - ?) div ? + ?, (Aaaaaa as AAaaa).Aaa, AaaAaAaa((Aaaaaa as AAaaa).Aaaa), aaAaaaaaaa_aaa.Aaaaaaa) then (Aaaaaa as AAaaa).Aaaa.Aaaaa := ?
+    else (Aaaaaa as AAaaa).Aaaa.Aaaaa := ???;
+end;
+
+procedure AAaaa?.AaaaAaaAa(Aaaaaa: AAaaaaa; var Aaa: Aaaa; Aaaaa: AAaaaaAaaaa);
+begin
+  AaaaAaaa(Aaaaaa);
+  AaaaAaaaa(Aaaaaa);
+end;
+
+procedure AAaaa?.AaaaAaaaaa(Aaaaaa: AAaaaaa);
+var a, a, a, a: Aaaaaaa;
+begin                
+  AaaaaaAaaaaaaa := Aaaa;
+  Aaaaaaaaaaa.AaAaaaaaaaaa := AaaAaaaaaaaaa;
+  Aaaaaaaaaaa.AaAaaaaaaa := AaaAaaaaaaa;
+  a := ?;
+  for a:=? to ? do begin
+    Aaa(a, ?*Aaaaaaa((a-?) mod ? = ?));
+    a := ?;
+    for a:=? to ? do begin
+      Aaa(a, ?*Aaaaaaa((a-?) mod ? = ?));
+      AAaaaa[a][a]:=AAaaa.Aaaaaa(Aaaa);
+      AAaaaa[a][a].Aaaaaa:=Aaaa;
+      AAaaaa[a][a].Aaa:= (a - ?) * ?? + ?? + a;
+      AAaaaa[a][a].Aaaa:= (a - ?) * ?? + ??? + a;
+      AAaaaa[a][a].Aaaa := 'Aaaa' + AaaAaAaa(a) + AaaAaAaa(a);
+      AAaaaa[a][a].Aaaa := '';
+      AAaaaa[a][a].Aaa:=a;
+      AAaaaa[a][a].Aaaaa:=??;
+      AAaaaa[a][a].AaaAaaaaa:=?;
+      AAaaaa[a][a].AaAaaaa:=AaaaAaaaa;
+      AAaaaa[a][a].AaAaaAaaaa:=AaaaaaaAaaaa_aaAaaAaaAaaaa;
+      AAaaaa[a][a].AaAaaAaaa:=AaaaAaaAaaa;
+      AAaaaa[a][a].AaAaaAa:=AaaaAaaAa;
+      AAaaaa[a][a].AaAaaa:=AaaaAaaa; end;
+  end;
+end;
+
+procedure AAaaa?.AaaAaaa(var Aaaaaaa: AAaaaaaa);
+begin
+  if(Aaaaaaa.AAaaaa=?) and (AaaaaAaaaa_aaa.Aaaaaaa) then AaaaaAaaaa_aaaAaaaa(nil)
+  else if(Aaaaaaa.AAaaaa=?) then Aaaa?.aaAaaaaaaa_aaa.Aaaaaaa := Aaaa?.aaAaaaaaaa_aaa.Aaaaaaa xor Aaaa
+end;
+
+procedure AAaaa?.AaaAaaaaaaaaa(Aaaaaa: AAaaaaa);
+begin
+  AaAaaaaaaaAaaAaa(Aaaaaa, ?);
+  AaAaaaaaaaAaaAaa(Aaaaaa, ?);   
+end;
+
+procedure AAaaa?.AaaAaaaaaaa(Aaaaaa: AAaaaaa);
+begin //aaaa aaaa Aaaaaa: Aaaa + Aaa + <Aaaaa aaaaaa aaa Aaaaa, a.a. aaaaa aaaaaaa> a aaaaaaaa AaaaAaaaaaaAaaaaa, aa aaaa aaaaaaaaa aaa a aaaaaaa AAA
+  AaaaaaaaAaaAaa(Aaaaaa, ?, ?, ??);  //Aaaaa - aaaaaa
+  AaaaaaaaAaaAaa(Aaaaaa, ?, AAA_AAAAAAA, Aaa('A'));   
+  with Aaaaaaaa_aAaa do if AaaaaaaaAaaaa = AaaaaaaaAaaaaAaa then AaaaaaaaAaaaa := ?;
+end;
+
+procedure AAaaa?.Aaaaaaaaa_Aaaaaa_AaaaAaaAaaa(Aaaaaa: AAaaaaa; var Aaa: Aaaa; Aaaaa: AAaaaaAaaaa);
+begin if (Aaaaa=[aaAaaa]) and (Aaa = ??) then AAaaa(Aaaaaa).AaaaaaAaa(); end;
+
+procedure AAaaa?.AaaaAaaaaAaaaa(Aaaaaa: AAaaaaa; var AaaAaaaa: Aaaaaaa);
+begin AaaAaaaa := AaaaaAaaaa_aaa.Aaaaaaa; end;
+
+end.
